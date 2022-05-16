de545dd3432556ecc94fe9cb4784253fb65aebff
danischm <danischm@cisco.com>
2022-05-16 17:45:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Catalyst 9000 EVPN Terraform Example
+
+This example demonstrates how the [IOS-XE Terraform Provider](https://registry.terraform.io/providers/netascode/iosxe/latest/docs) can be used to build a Catalyst 9000 EVPN Fabric. It currently supports underlay and overlay configuration, but no access interfaces.
+
+It uses the following Terrafrom Modules:
+
+- [EVPN OSPF Underlay Module](https://registry.terraform.io/modules/netascode/evpn-ospf-underlay/iosxe/latest)
+- [EVPN Overlay Module](https://registry.terraform.io/modules/netascode/evpn-overlay/iosxe/latest)
+
+The configuration is derived from a set of yaml files in the `data` [directory](https://github.com/netascode/terraform-iosxe-evpn-example/tree/main/data).
+
+To point this to your own Cat9k fabric, update the `data/inventory.yaml` file accordingly.
+
+```yaml
+---
+fabric:
+  inventory:
+    spines:
+      - name: SPINE-1
+        url: https://10.1.1.1
+      - name: SPINE-2
+        url: https://10.1.1.2
+
+    leafs:
+      - name: LEAF-1
+        url: https://10.1.1.3
+      - name: LEAF-2
+        url: https://10.1.1.4
+```
+
+Credentials can either be provided via environment variables:
+
+```shell
+export IOSXE_USERNAME=admin
+export IOSXE_PASSWORD=Cisco123
+```
+
+Or by updating the provider configuration in `main.tf`:
+
+```terraform
+provider "iosxe" {
+  username = admin
+  password = Cisco123
+  devices  = local.devices
+}
+```
---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    iosxe = {
+      source  = "netascode/iosxe"
+      version = ">=0.1.7"
+    }
+    utils = {
+      source  = "netascode/utils"
+      version = ">= 0.1.1"
+    }
+  }
+}
+
+locals {
+  model = yamldecode(data.utils_yaml_merge.model.output)
+
+  devices = concat(lookup(local.model.fabric.inventory, "leafs", []), lookup(local.model.fabric.inventory, "spines", []))
+  leafs   = toset([for leaf in lookup(local.model.fabric.inventory, "leafs", []) : leaf.name])
+  spines  = toset([for spine in lookup(local.model.fabric.inventory, "spines", []) : spine.name])
+}
+
+provider "iosxe" {
+  devices = local.devices
+}
+
+data "utils_yaml_merge" "model" {
+  input = [for file in fileset(path.module, "data/*.yaml") : file(file)]
+}
+
+module "iosxe_evpn_ospf_underlay" {
+  source  = "netascode/evpn-ospf-underlay/iosxe"
+  version = ">= 0.1.0"
+
+  leafs                         = local.leafs
+  spines                        = local.spines
+  loopback_id                   = lookup(local.model.fabric.underlay, "loopback_id", null)
+  pim_loopback_id               = lookup(local.model.fabric.underlay, "pim_loopback_id", null)
+  loopbacks                     = lookup(local.model.fabric.underlay, "loopbacks", null)
+  vtep_loopback_id              = lookup(local.model.fabric.underlay, "vtep_loopback_id", null)
+  vtep_loopbacks                = lookup(local.model.fabric.underlay, "vtep_loopbacks", null)
+  fabric_interface_type         = lookup(local.model.fabric.underlay, "fabric_interface_type", null)
+  leaf_fabric_interface_prefix  = lookup(local.model.fabric.underlay, "leaf_fabric_interface_prefix", null)
+  spine_fabric_interface_prefix = lookup(local.model.fabric.underlay, "spine_fabric_interface_prefix", null)
+  leaf_fabric_interface_offset  = lookup(local.model.fabric.underlay, "leaf_fabric_interface_offset", null)
+  spine_fabric_interface_offset = lookup(local.model.fabric.underlay, "spine_fabric_interface_offset", null)
+  anycast_rp_ipv4_address       = lookup(local.model.fabric.underlay, "anycast_rp_ipv4_address", null)
+}
+
+module "iosxe_evpn_overlay" {
+  source  = "netascode/evpn-overlay/iosxe"
+  version = ">= 0.1.0"
+
+  leafs                = local.leafs
+  spines               = local.spines
+  underlay_loopback_id = module.iosxe_evpn_ospf_underlay.loopback_id
+  underlay_loopbacks   = module.iosxe_evpn_ospf_underlay.loopbacks
+  vtep_loopback_id     = module.iosxe_evpn_ospf_underlay.vtep_loopback_id
+  bgp_asn              = lookup(local.model.fabric.overlay, "bgp_asn", null)
+  l2_services          = lookup(local.model.fabric.overlay, "l2_services", null)
+  l3_services          = lookup(local.model.fabric.overlay, "l3_services", null)
+}
